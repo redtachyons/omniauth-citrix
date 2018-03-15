1343079eab5597c524e045a1908a057912894961
Aboobacker MK <aboobacker@redpanthers.co>
2018-03-15 17:44:45 +0530

    Update citrix.rb

--- lib/omniauth/strategies/citrix.rb
+++ lib/omniauth/strategies/citrix.rb
@@ -1,56 +1,57 @@
-require "omniauth"
-require "omniauth-oauth2"
+require 'omniauth'
+require 'omniauth-oauth2'
 
 module OmniAuth
   module Strategies
     class Citrix < OmniAuth::Strategies::OAuth2
-      option :client_options, {
-        authorize_url: "https://api.getgo.com/oauth/v2/authorize",
-        token_url: "https://api.getgo.com/oauth/v2/access_token"
-      }
+      option :client_options,
+             authorize_url: 'https://api.getgo.com/oauth/v2/authorize',
+             token_url: 'https://api.getgo.com/oauth/v2/token'
 
       option :provider_ignores_state, true
 
       def authorize_params
-        {client_id: client.id}
+        { client_id: client.id }
       end
 
       def token_params
         {
-          "grant_type" => "authorization_code",
-          "code" => request.params["code"],
-          "client_id" => client.id
+          'grant_type' => 'authorization_code',
+          'code' => request.params['code'],
+          'client_id' => client.id
         }
       end
 
       uid do
-        access_token.params["account_key"]
+        access_token.params['account_key']
       end
 
       info do
         {
-          organizer_key: access_token.params["organizer_key"],
-          account_key: access_token.params["account_key"],
-          account_type: access_token.params["account_type"],
-          first_name: access_token.params["firstName"].strip,
-          last_name: access_token.params["lastName"].strip,
-          email: access_token.params["email"],
-          platform: access_token.params["platform"]
+          organizer_key: access_token.params['organizer_key'],
+          account_key: access_token.params['account_key'],
+          account_type: access_token.params['account_type'],
+          first_name: access_token.params['firstName'].strip,
+          last_name: access_token.params['lastName'].strip,
+          email: access_token.params['email'],
+          platform: access_token.params['platform']
         }
       end
-      
+
       def request_phase
         redirect client.auth_code.authorize_url(authorize_params)
       end
 
       protected
 
+      def base64_auth
+        'Basic ' + Base64.encode64("#{client.id}:#{client.secret}").delete("\n")
+      end
+
       def build_access_token
-        client.auth_code.get_token(
-          request.params["code"],
-          {"redirect_uri" => callback_url}.merge(token_params),
-          options.auth_token_params
-        )
+        client.auth_code.get_token(request.params['code'],
+                                   headers: { 'Authorization' => base64_auth },
+                                   token_method: :post)
       end
     end
   end
